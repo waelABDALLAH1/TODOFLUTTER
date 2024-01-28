61bf0c4b89f8d4096fb46e8110cff05e35303c9d
wael <wael.abdallah@etudiant-enit.utm.tn>
2024-01-28 13:11:57 +0100

    bdit fl todo w jawna behy

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:todo/auth/authscreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo/screens/add_task.dart';
+
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -6,11 +11,39 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+void _signOut() async {
+  try {
+    await _auth.signOut();
+    print('User signed out');
+  } catch (e) {
+    print('Error signing out: $e');
+  }
+}
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('TODO'),backgroundColor: Colors.green[300],),
+      appBar: AppBar(
+        title: Center(child: Text('TODO')),
+        backgroundColor: Colors.green[300],
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child:  ElevatedButton(
+          onPressed: _signOut,
+          child: Text('Sign Out')
+          ,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add, color: Colors.white,),
+          backgroundColor: Theme.of(context).primaryColor, onPressed: () {
+          Navigator.push(context,MaterialPageRoute(builder: (context)=>AddTask()));
+      }),
     );
   }
 }
---
+++ lib/screens/add_task.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class AddTask extends StatefulWidget {
+  const AddTask({super.key});
+
+  @override
+  State<AddTask> createState() => _AddTaskState();
+}
+
+class _AddTaskState extends State<AddTask> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Task'),
+      ),
+    );
+  }
+}
