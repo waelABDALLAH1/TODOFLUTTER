d72010606c29f705e80bc6c38a5b5362a42be2b3
wael <wael.abdallah@etudiant-enit.utm.tn>
2024-01-28 17:47:07 +0100

    wslet lil  description

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:todo/auth/authscreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:todo/screens/add_task.dart';
-
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -23,6 +26,28 @@ void _signOut() async {
 }
 
 class _HomeState extends State<Home> {
+  String uid = '';
+
+  @override
+  void initState() {
+    getUid();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void getUid() {
+    final User? user = _auth.currentUser;
+    setState(() {
+      uid = user?.uid ?? '';
+    });
+
+    if (uid.isEmpty) {
+      // Handle the case where uid is empty, for example, by showing an error message.
+      print('Error: User UID is empty.');
+      // You may want to handle this error case appropriately for your application.
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,19 +56,98 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.green[300],
       ),
       body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child:  ElevatedButton(
-          onPressed: _signOut,
-          child: Text('Sign Out')
-          ,
+        height: MediaQuery
+            .of(context)
+            .size
+            .height,
+        width: MediaQuery
+            .of(context)
+            .size
+            .width,
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('tasks')
+              .doc(uid)
+              .collection('mytasks')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              final docs = snapshot.data?.docs ?? [];
+
+              return ListView.builder(
+                itemCount: docs.length,
+                itemBuilder: (context, index) {
+                  var time = (docs[index]['timestamp'] as Timestamp)
+                      .toDate();
+                  return InkWell(
+                    onTap: (){},
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Color(0xff121211),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      height: 90,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                  margin: EdgeInsets.only(left: 20),
+                                  child: Text(
+                                    docs[index]['title'],
+                                    style: GoogleFonts.roboto(fontSize: 20),
+                                  )),
+                              SizedBox(height: 5,),
+                              Container(
+                                margin: EdgeInsets.only(left: 20),
+                                child: Text(DateFormat.yMd().add_jm().format(time)),
+                              )
+                            ],
+                          ),
+                          Container(
+                            child: IconButton(
+                                onPressed: () async {
+                                  await FirebaseFirestore.instance
+                                      .collection('tasks')
+                                      .doc(uid)
+                                      .collection('mytasks')
+                                      .doc(docs[index]['time'])
+                                      .delete();
+                                },
+                                icon: Icon(
+                                  Icons.delete,
+                                )),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add, color: Colors.white,),
-          backgroundColor: Theme.of(context).primaryColor, onPressed: () {
-          Navigator.push(context,MaterialPageRoute(builder: (context)=>AddTask()));
-      }),
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          backgroundColor: Theme
+              .of(context)
+              .primaryColor,
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => AddTask()));
+          }),
     );
   }
 }
--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -1,4 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:toast/toast.dart';
+
+void showCustomToast(BuildContext context, String message) {
+  final snackBar = SnackBar(
+    content: Text(message),
+    duration: Duration(seconds: 2), // Durée d'affichage du toast
+  );
+
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
 class AddTask extends StatefulWidget {
   const AddTask({super.key});
 
@@ -7,11 +21,97 @@ class AddTask extends StatefulWidget {
 }
 
 class _AddTaskState extends State<AddTask> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  void addTaskToFirebase() async {
+    try {
+      // Get the current user
+      FirebaseAuth auth = FirebaseAuth.instance;
+      final User? user = auth.currentUser;
+      String uid = user?.uid ?? '';
+
+      // Get the current timestamp
+      var time = DateTime.now();
+
+      // Add task to Firestore
+      await FirebaseFirestore.instance
+          .collection('tasks') // Main collection
+          .doc(uid) // User's document
+          .collection('mytasks') // Subcollection for tasks
+          .doc(time.toString()) // Document ID is set to the current timestamp
+          .set({
+        'title': titleController.text,
+        'description': descriptionController.text,
+        'time': time.toString(),
+        'timestamp': time,
+      });
+
+      // Show a toast message using the toast package
+      showCustomToast(context, 'Task Added');
+    } catch (e) {
+      // Handle errors
+      print('Error adding task: $e');
+      showCustomToast(context, 'Error adding task');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('New Task'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Container(
+              child: TextField(
+                controller: titleController,
+                decoration: InputDecoration(
+                    labelText: 'Enter Title ', border: OutlineInputBorder()),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              child: TextField(
+                controller: descriptionController,
+                decoration: InputDecoration(
+                    labelText: 'Enter Description  ',
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return Colors.blueAccent.shade100;
+                      }
+                      return Theme.of(context).primaryColor;
+                    },
+                  ),
+                ),
+                onPressed: () {
+                  addTaskToFirebase();
+                },
+                child: Text(
+                  'Add Task',
+                  style: GoogleFonts.roboto(fontSize: 18),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
